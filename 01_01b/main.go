538338320ec79b7f069ae98036f299dcadad4f9f
Keane Moraes <lordvader3002@gmail.com>
2022-12-31 10:20:17 +0000

    my solution for 01_01

--- 01_01b/main.go
+++ 01_01b/main.go
@@ -10,12 +10,27 @@ var expectedFormat = "2006-01-02"
 
 // parseTime validates and parses a given date string.
 func parseTime(target string) time.Time {
-	panic("NOT IMPLEMENTED")
+	// panic("NOT IMPLEMENTED")
+	parsedTimed, err := time.Parse(expectedFormat, target)
+	if err != nil { log.Fatal("ERROR: " + err.Error()) }
+
+	// If the target time is before the current time, throw an error
+	if time.Now().After(parsedTimed) {
+		log.Fatal("ERROR: provided time is before the current time")
+	}
+
+	return parsedTimed
 }
 
 // calcSleeps returns the number of sleeps until the target.
 func calcSleeps(target time.Time) float64 {
-	panic("NOT IMPLEMENTED")
+	
+	// Get the duration from current time upto to the specified target
+	duration := time.Until(target)
+
+	// Convert the nanoseconds into days.
+	daysUntilNext := duration.Hours() / 24
+	return daysUntilNext
 }
 
 func main() {
